91069623fc1fecb6453fc428173436b605b1a898
KiryhaVlasov <m45362388@gmail.com>
2022-05-25 22:11:27 +0300

    Добавьте файлы проекта.

---
+++ task29/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("введите длину массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[n];
+void FillArray(int[] massive)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.WriteLine($"введите {i+1} цифру массива ");
+        mass[i] =int.Parse(Console.ReadLine());
+    }
+}
+void PrintArray(int[] massive)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write($"{mass[i]} ");
+        
+    }
+}
+
+FillArray(mass);
+Console.Write("[");
+PrintArray(mass);
+Console.Write("]");
